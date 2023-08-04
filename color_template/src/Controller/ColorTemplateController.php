262a5ca103534a43640e7b460899b7539fa8d4ba
Satyam Prakash <satyamprakash25@gmail.com>
2023-08-04 10:20:06 +0530

    added color template task

---
+++ color_template/src/Controller/ColorTemplateController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\color_template\Controller;
+
+use Drupal\Component\DependencyInjection\Container;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Returns responses for Color Template Task Module Module routes.
+ */
+class ColorTemplateController extends ControllerBase {
+
+  protected $configFactory;
+
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static (
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * Builds the response.
+   */
+  public function build() {
+
+    $config = $this->configFactory->getEditable('color_template.settings');
+    $title = $config->get('title');
+    $text = strip_tags($config->get('paragraph')['value']);
+    $color = $config->get('color');
+
+    return [
+      '#theme' => 'custom_template',
+      '#title' => $title,
+      '#text' => $text,
+      '#color' => $color,
+    ];
+  }
+
+}
---
+++ color_template/src/Form/SettingsForm.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\color_template\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure Color Template Task Module for this site.
+ */
+class SettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'color_template_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['color_template.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Title'),
+      '#default_value' => $this->config('color_template.settings')->get('title'),
+    ];
+
+
+    $text_format = 'basic_html';
+    if ($this->config('color_template.settings')->get('paragraph')['format']) {
+      $text_format = $this->config('template_task.settings')->get('paragraph')['format'];
+    }
+
+    $form['paragraph'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Paragraph'),
+      '#format' => $text_format,
+      '#default_value' => $this->config('color_template.settings')->get('paragraph')['value'],
+    ];
+
+    $form['color'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Color Code'),
+      '#default_value' => $this->config('color_template.settings')->get('color'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('color_template.settings')
+      ->set('title', $form_state->getValue('title'))
+      ->set('paragraph', $form_state->getValue('paragraph'))
+      ->set('color', $form_state->getValue('color'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
